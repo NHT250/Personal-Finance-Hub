0d66d71a4fb449cdae7d9acecf7c36c2ee98dee7
NHT250 <tienkg0946@gmail.com>
2026-03-27 02:13:03 +0700

    Delete backend directory

--- backend/app/utils/response.py
+++
@@ -1,6 +0,0 @@
-def ok(data=None, message='Thành công', code=200):
-    return {'success': True, 'message': message, 'data': data}, code
-
-
-def fail(message='Có lỗi xảy ra', code=400):
-    return {'success': False, 'message': message}, code
